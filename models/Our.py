040c8777e71d0804122d098f01404df9de256633
zhengxianda <zhengxianda@outlook.com>
2019-03-13 16:03:08 +0800

    update our algorithm

--- models/Our.py
+++ models/Our.py
@@ -14,7 +14,6 @@ class Our(Model):
         self.ent_embeddings = nn.Embedding(self.config.entTotal, self.config.hidden_size)
         # self.rel_embeddings = nn.Embedding(self.config.relTotal, self.config.hidden_size)
         self.ent_transfer = nn.Embedding(self.config.entTotal, self.config.hidden_size)
-        self.rel_transfer = nn.Embedding(self.config.relTotal, self.config.hidden_size)
         self.criterion = nn.MarginRankingLoss(self.config.margin, False)
         self.init_weights()
 
@@ -22,27 +21,35 @@ class Our(Model):
         nn.init.xavier_uniform(self.ent_embeddings.weight.data)
         # nn.init.xavier_uniform(self.rel_embeddings.weight.data)
         nn.init.xavier_uniform(self.ent_transfer.weight.data)
-        nn.init.xavier_uniform(self.rel_transfer.weight.data)
+
+    def _transfer(self, e, e_transfer, e2_transfer):
+        e = e + torch.sum(e * e_transfer, -1, True) * e2_transfer
+        e_norm = F.normalize(e, p=2, dim=-1)
+        return e_norm
 
     def _calc(self, h, t):
-        # print(torch.norm(h, dim=1))
-        # print(torch.norm(h))
-        # print(torch.norm(self.m, dim=1))
-        # print("r")
-        # print(self.batch_h)
-        # print(self.batch_t)
-        # print(r)
-        # a = 1.0 + (r.to(torch.float32) - 0.5) * 2 * torch.cosine_similarity(h, t) + (r.to(
-        #     torch.float32) - 0.5) * -2 * torch.norm(m, dim=1) - torch.norm(n, dim=1)
-        # print(a)
-        # print("r change")
-        # print((r.to(torch.float32) - 0.5) * 2)
-        # h_r_t = torch.norm(h + r - t, self.config.p_norm, -1)
-        # trans_e = (self.batch_r.to(torch.float32) - 0.5) * 2 * h_r_t
+        # ans = Variable(torch.from_numpy(np.zeros(len(t), dtype=np.float32).cuda()), requires_grad=False)
+        # print(ans.size)
+        # ans = ans.float()
+        # ans = ans.cuda()
+        # ans = Variable(ans)
+        # print(ans.size())
+        # print(h.size())
+        # ans = 1.0 + (self.batch_r.to(torch.float32) - 1.0) * torch.cosine_similarity(h_sub, t_sub) + (
+        #     self.batch_r.to(torch.float32)) * torch.cosine_similarity(h_dis, t_dis)
+        # for i in range(len(ans)):
+        #     # print(torch.cosine_similarity(h_sub[i], t_sub[i]))
+        #     # print(h_sub[i].size())
+        #     # print(t_sub[i].size())
+        #     # print(torch.cosine_similarity(h_sub[i], t_sub[i], dim=0))
+        #     if self.batch_r[i] == 0:  # sub
+        #         ans[i] = 1.0 - torch.cosine_similarity(h_sub[i], t_sub[i], dim=0)
+        #         # print(ans[i])
+        #     if self.batch_r[i] == 1:  # dis
+        #         ans[i] = 1.0 + torch.cosine_similarity(h_dis[i], t_dis[i], dim=0)
+        #         # print(ans[i])
+        # return ans
         cos = 1.0 + (self.batch_r.to(torch.float32) - 0.5) * 2 * torch.cosine_similarity(h, t)
-        # print(self.batch_r)
-        # print((self.batch_r.to(torch.float32) - 0.5) * 2)
-        # print(torch.cosine_similarity(h, t))
         return cos
         # return 1.0 + (r.to(torch.float32) - 0.5) * 2 * torch.cosine_similarity(h, t) + (
         #             r.to(torch.float32) - 0.5) * -2 * torch.norm(h, dim=1) - torch.norm(t, dim=1)
@@ -52,11 +59,6 @@ class Our(Model):
         # return 1.0 + (r.to(torch.float32) - 0.5) * 2 * torch.cosine_similarity(h, t) + (
         #             r.to(torch.float32) - 0.5) * -2 * torch.norm(m) - torch.norm(n)
 
-    def _transfer(self, e, e_transfer, r_transfer):
-        e = e + torch.sum(e * e_transfer, -1, True) * r_transfer
-        e_norm = F.normalize(e, p=2, dim=-1)
-        return e_norm
-
     def loss(self, p_score, n_score):
         y = Variable(torch.Tensor([-1]).cuda())
         return self.criterion(p_score, n_score, y)
@@ -64,12 +66,11 @@ class Our(Model):
     def forward(self):
         h = self.ent_embeddings(self.batch_h)
         t = self.ent_embeddings(self.batch_t)
-        # r = self.rel_embeddings(self.batch_r)
+        # r = self.batch_r
         h_transfer = self.ent_transfer(self.batch_h)
         t_transfer = self.ent_transfer(self.batch_t)
-        r_transfer = self.rel_transfer(self.batch_r)
-        h = self._transfer(h, h_transfer, r_transfer)
-        t = self._transfer(t, t_transfer, r_transfer)
+        h = self._transfer(h, h_transfer, t_transfer)
+        t = self._transfer(t, t_transfer, h_transfer)
         score = self._calc(h, t)
         p_score = self.get_positive_score(score)
         n_score = self.get_negative_score(score)
@@ -78,11 +79,10 @@ class Our(Model):
     def predict(self):
         h = self.ent_embeddings(self.batch_h)
         t = self.ent_embeddings(self.batch_t)
-        # r = self.rel_embeddings(self.batch_r)
+        # r = self.batch_r
         h_transfer = self.ent_transfer(self.batch_h)
         t_transfer = self.ent_transfer(self.batch_t)
-        r_transfer = self.rel_transfer(self.batch_r)
-        h = self._transfer(h, h_transfer, r_transfer)
-        t = self._transfer(t, t_transfer, r_transfer)
+        h = self._transfer(h, h_transfer, t_transfer)
+        t = self._transfer(t, t_transfer, h_transfer)
         score = self._calc(h, t)
         return score.cpu().data.numpy()
